8796d983259fff15cc89276f5f4fb0dcc8fb448e
Lance W <50839441+disposableraft@users.noreply.github.com>
2019-09-21 08:23:42 -0700

    Adds the fields for CSV import to model

--- app/server/mono/curator/models.py
+++ app/server/mono/curator/models.py
@@ -1,15 +1,38 @@
 from django.db import models
 
+"""
+csv_cols = [
+    'ExhibitionID',
+    'ExhibitionNumber',
+    'ExhibitionTitle',
+    'ConstituentURL', 
+    'FirstName',
+    'MiddleName',
+    'LastName',
+    'Suffix',
+    'ExhibitionURL',
+    'ExhibitionRole',
+    'DisplayName',
+]
+"""
+
 class Exhibition(models.Model):
+    moma_id = models.IntegerField()
+    moma_number = models.CharField(max_length=50)
+    moma_url = models.URLField()
     title = models.CharField(max_length=200)
 
     def __str__(self):
         return self.title
 
 class Artist(models.Model):
+    display_name = models.CharField(max_length=200, null=True)
     exhibition = models.ForeignKey(Exhibition, on_delete=models.CASCADE)
-    last_name = models.CharField(max_length=200)
     first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    middle_name = models.CharField(max_length=200, null=True)
+    moma_url = models.URLField()
+    suffix = models.CharField(max_length=25, null=True)
     token = models.CharField(max_length=200)
 
     def full_name(self):
