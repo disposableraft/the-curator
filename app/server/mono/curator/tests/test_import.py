d76f19bf0259bef478c2552e52c7ab5c58fd95c5
Lance W <50839441+disposableraft@users.noreply.github.com>
2019-09-22 07:44:37 -0700

    Add importer tests
    - Exhibitions with one artist are showing zero artists

--- app/server/mono/curator/tests/test_import.py
+++ app/server/mono/curator/tests/test_import.py
@@ -3,10 +3,27 @@ from django.conf import settings
 from io import StringIO
 from django.core.management import call_command
 from django.test import TestCase
+from curator.models import Exhibition, Artist
 
 class ImportTest(TestCase):
+    csv = os.path.join(settings.BASE_DIR, 'curator/tests/import_test.csv') 
+    
     def test_command_output(self):
         out = StringIO()
-        csv = os.path.join(settings.BASE_DIR, 'curator/tests/import_test.csv') 
-        call_command('import', csv, stdout=out)
-        self.assertIn('Imported 3 Exhibitions and 4 Artists', out.getvalue())
+        call_command('import', self.csv, stdout=out)
+        self.assertIn('Imported 4 Exhibitions and 5 Artists', out.getvalue())
+    
+    def test_command_creates_single_artist(self):
+        call_command('import', self.csv)
+        warhol = Exhibition.objects.get(moma_number=1510)
+        self.assertEqual(1, warhol.artist_set.count())
+    
+    def test_command_creates_5_artists(self):
+        call_command('import', self.csv)
+        count = Artist.objects.count()
+        self.assertEqual(5, count)
+    
+    def test_command_creates_4_exhibitions(self):
+        call_command('import', self.csv)
+        count = Exhibition.objects.count()
+        self.assertEqual(4, count)
