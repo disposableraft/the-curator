dcdd0e35c0fca4140d4df5b302d211d613822041
Lance W <50839441+disposableraft@users.noreply.github.com>
2019-09-22 08:36:44 -0700

    Add a basic admin interface

--- app/server/mono/curator/admin.py
+++ app/server/mono/curator/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Artist, Exhibition
+
+
+class ArtistInline(admin.TabularInline):
+    fields = ('display_name', 'token', 'moma_url')
+    model = Artist
+    extra = 0
+
+
+class ExhibitionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['title', 'moma_url']}),
+        ('Meta', {
+            'fields': ['moma_id', 'moma_number'],
+            'classes': ['collapse'],
+        }),
+    ]
+    search_fields = ['title']
+    list_display = [
+        'title', 'artist_count', 'moma_url',
+    ]
+    inlines = [ArtistInline]
+    def artist_count(self, obj):
+        return len(obj.artist_set.all())
+
+admin.site.register(Exhibition, ExhibitionAdmin)
