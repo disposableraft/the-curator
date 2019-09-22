184ffdeef2a90b83de04339f0efe0533a65d6e90
Lance W <50839441+disposableraft@users.noreply.github.com>
2019-09-22 08:09:28 -0700

    Reproduce Warhol bug:
    - A single artists is not added to an exhibition
      when that artist has already been created.

--- app/server/mono/curator/tests/test_import.py
+++ app/server/mono/curator/tests/test_import.py
@@ -11,19 +11,27 @@ class ImportTest(TestCase):
     def test_command_output(self):
         out = StringIO()
         call_command('import', self.csv, stdout=out)
-        self.assertIn('Imported 4 Exhibitions and 5 Artists', out.getvalue())
-    
-    def test_command_creates_single_artist(self):
-        call_command('import', self.csv)
-        warhol = Exhibition.objects.get(moma_number=1510)
-        self.assertEqual(1, warhol.artist_set.count())
-    
+        self.assertIn('Imported 5 Exhibitions and 5 Artists', out.getvalue())
+        
     def test_command_creates_5_artists(self):
         call_command('import', self.csv)
         count = Artist.objects.count()
         self.assertEqual(5, count)
     
-    def test_command_creates_4_exhibitions(self):
+    def test_command_creates_5_exhibitions(self):
         call_command('import', self.csv)
         count = Exhibition.objects.count()
-        self.assertEqual(4, count)
+        self.assertEqual(5, count)
+    
+    def test_command_links_artists_to_exhibitions(self):
+        call_command('import', self.csv)
+        a = Exhibition.objects.get(title='Exhibition A')
+        b = Exhibition.objects.get(title='Exhibition B')
+        c = Exhibition.objects.get(title='Exhibition C')
+        d = Exhibition.objects.get(title='Exhibition D')
+        e = Exhibition.objects.get(title='Exhibition E')
+        self.assertEqual(3, a.artist_set.count())
+        self.assertEqual(2, b.artist_set.count())
+        self.assertEqual(3, c.artist_set.count())
+        self.assertEqual(1, d.artist_set.count())
+        self.assertEqual(1, e.artist_set.count())
