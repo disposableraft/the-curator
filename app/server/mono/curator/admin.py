68ff8ac7841076af892d53e6eac5ca8984db424f
Lance W <50839441+disposableraft@users.noreply.github.com>
2019-09-23 15:26:11 -0700

    Upgrade the admin interface:
    - Disallow editing via manytomany field
    - New search fields
    - Artist list display

--- app/server/mono/curator/admin.py
+++ app/server/mono/curator/admin.py
@@ -6,22 +6,33 @@ class MembershipInline(admin.TabularInline):
     model = Artist.exhibition.through
     extra = 0
     can_delete = False
+    classes = ['collapse']
+
+    def has_change_permission(self, request, obj=None):
+        return False
 
 
 class ArtistAdmin(admin.ModelAdmin):
     inlines = [MembershipInline]
     exclude = ('exhibition',)
+    search_fields = ['display_name', 'token']
+    fields = ('display_name', 'token', 'moma_url')
+    list_display = [
+        'display_name', 'exhibition_count', 'moma_url', 'token',
+    ]
 
+    def exhibition_count(self, obj):
+        return obj.exhibition.count()
 
 class ExhibitionAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None, {'fields': ['title', 'moma_url']}),
+        (None, {'fields': ['title', 'moma_url',]}),
         ('Meta', {
-            'fields': ['moma_id', 'moma_number'],
+            'fields': ['moma_id', 'moma_number',],
             'classes': ['collapse'],
         }),
     ]
-    search_fields = ['title']
+    search_fields = ['title', 'moma_number']
     list_display = [
         'title', 'artist_count', 'moma_url',
     ]
