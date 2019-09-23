5b941b439d1b02838644c51f0ed407c62302d7fb
Lance W <50839441+disposableraft@users.noreply.github.com>
2019-09-23 09:40:30 -0700

    Standardize test calls to import command:
    - Prevents stdout from polluting console during testing

--- app/server/mono/curator/tests/test_import.py
+++ app/server/mono/curator/tests/test_import.py
@@ -5,26 +5,32 @@ from django.core.management import call_command
 from django.test import TestCase
 from curator.models import Exhibition, Artist
 
+
+def import_command(csv):
+    out = StringIO()
+    call_command('import', csv, stdout=out)
+    return out
+
+
 class ImportTest(TestCase):
-    csv = os.path.join(settings.BASE_DIR, 'curator/tests/import_test.csv') 
-    
+    csv = os.path.join(settings.BASE_DIR, 'curator/tests/import_test.csv')
+
     def test_command_output(self):
-        out = StringIO()
-        call_command('import', self.csv, stdout=out)
+        out = import_command(self.csv)
         self.assertIn('Imported 5 Exhibitions and 5 Artists', out.getvalue())
-        
+
     def test_command_creates_5_artists(self):
-        call_command('import', self.csv)
+        out = import_command(self.csv)
         count = Artist.objects.count()
         self.assertEqual(5, count)
-    
+
     def test_command_creates_5_exhibitions(self):
-        call_command('import', self.csv)
+        out = import_command(self.csv)
         count = Exhibition.objects.count()
         self.assertEqual(5, count)
-    
+
     def test_command_links_artists_to_exhibitions(self):
-        call_command('import', self.csv)
+        out = import_command(self.csv)
         a = Exhibition.objects.get(title='Exhibition A')
         b = Exhibition.objects.get(title='Exhibition B')
         c = Exhibition.objects.get(title='Exhibition C')
