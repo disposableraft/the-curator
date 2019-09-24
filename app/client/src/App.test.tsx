0b0d494d98b7047a21021c183aacb93f797c6596
Lance W <50839441+disposableraft@users.noreply.github.com>
2019-09-24 08:43:42 -0700

    Update package.json and fix failing tests

--- app/client/src/App.test.tsx
+++ app/client/src/App.test.tsx
@@ -7,7 +7,7 @@ configure({ adapter: new Adapter() });
 
 describe('<App>', () => {
   const defaultState = {
-    subject: 'Tadanori Yokoo',
+    subject: 'Foo Name Baz',
     images: [
       'https://www.moma.org/media/W1siZiIsIjkyODUiXSxbInAiLCJjb252ZXJ0IiwiLXJlc2l6ZSA1MTJ4NTEyXHUwMDNlIl1d.jpg?sha=bcbbfb73f150d6e2',
       'https://www.moma.org/media/W1siZiIsIjkyOTAiXSxbInAiLCJjb252ZXJ0IiwiLXJlc2l6ZSA1MTJ4NTEyXHUwMDNlIl1d.jpg?sha=a6282babc21b9ca9',
@@ -17,14 +17,13 @@ describe('<App>', () => {
 
   it('renders renders Artist Name', () => {
     const wrapper = shallow(<App />);
-    wrapper.this.state = defaultState;
+    wrapper.setState(defaultState);
     expect(wrapper.text()).toBe(defaultState.subject);
   });
   
   it('renders one or more images', () => {
     const wrapper = shallow(<App />);
-    expect(wrapper.text()).toBe(<Images />);
-    // expect(wrapper.find('.Images').children().length).toBeGreaterThanOrEqual(1);
+    expect(wrapper.find('.picture__img--static').length).toEqual(3);
   });
 });
 
