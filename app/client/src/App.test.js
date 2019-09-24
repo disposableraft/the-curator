c0007de17e83cece63b29ceae33a6bbf65fa5130
Lance W <50839441+disposableraft@users.noreply.github.com>
2019-09-24 16:17:24 -0700

    Install Enzyme

--- app/client/src/App.test.js
+++ app/client/src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+configure({ adapter: new Adapter() });
+
+describe('<App />', () => {
+  it('has a header class', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find('.App-header').length).toEqual(1)
+  });
 });
