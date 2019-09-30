1ac3a4329da6da556d8812ed7eec75b037f7a37e
Lance W <50839441+disposableraft@users.noreply.github.com>
2019-09-29 18:57:52 -0700

    Wires up basic react page:
    - Get data from Django
    - Display title and list of artists
    - Tests

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -1,12 +1,54 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="App-header">Artist</div>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.getData = this.getData.bind(this);
+    this.state = {
+      exhibition: {
+        title: '',
+        artists: [],
+      },
+    };
+  }
+
+  componentDidMount(){
+    this.getData()
+  }
+
+  getData() {
+    axios.get('http://127.0.0.1:8000/curator/exhibition/23/')
+    .then((res) => {
+      this.setState(state => {
+        return {
+          exhibition: res.data,
+        };
+      })
+    })
+    .catch((error) => {
+      console.error(error);
+    })
+  }
+
+  render() {
+    const {title, artists} = this.state.exhibition;
+    return (
+      <div className="App">
+        <div className="App-header">{title}</div>
+        <div className="App-artists">
+          <ul>
+            {artists.map(a => {
+              return (
+                <li key={a.token} className="artist">{a.display_name}</li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    );
+  };
 }
 
 export default App;
--- app/client/src/App.test.js
+++ app/client/src/App.test.js
@@ -1,13 +1,44 @@
 import React from 'react';
 import App from './App';
+import axios from 'axios';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
+jest.mock('axios')
+
+const res = {
+  data: {
+    artists: [{"display_name": "Georges-Pierre Seurat", "token": "georgpierrseurat", "moma_url": "moma.org/artists/5358"}, {"display_name": "Paul C\u00e9zanne", "token": "paulc\u00e9zann", "moma_url": "moma.org/artists/1053"}, {"display_name": "Paul Gauguin", "token": "paulgauguin", "moma_url": "moma.org/artists/2098"}, {"display_name": "Vincent van Gogh", "token": "vincentvangogh", "moma_url": "moma.org/artists/2206"}],
+    errors: null,
+    title: "Cézanne, Gauguin, Seurat, Van Gogh",
+    url: "moma.org/calendar/exhibitions/1767"
+  }
+}
+
 describe('<App />', () => {
-  it('has a header class', () => {
+  it('renders a header element', () => {
+    axios.get.mockResolvedValue(res)
     const wrapper = shallow(<App />)
     expect(wrapper.find('.App-header').length).toEqual(1)
+  });
+
+  it('sets the state using getData()', async () => {
+    axios.get.mockResolvedValue(res)
+    const wrapper = await shallow(<App />)
+    expect(wrapper.state().exhibition.title).toEqual('Cézanne, Gauguin, Seurat, Van Gogh')
+  });
+
+  it('displays a title', async () => {
+    axios.get.mockResolvedValue(res)
+    const wrapper = await shallow(<App />)
+    expect(wrapper.find('.App-header').text()).toContain('Cézanne, Gauguin, Seurat, Van Gogh')
+  });
+
+  it('renders a list of artists', async () => {
+    axios.get.mockResolvedValue(res)
+    const wrapper = await shallow(<App />);
+    expect(wrapper.find('ul').children().length).toEqual(4)
   });
 });
