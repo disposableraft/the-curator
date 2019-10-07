6d1cdd756d3190f06e0e58f7015bfac265b38832
Lance W <50839441+disposableraft@users.noreply.github.com>
2019-10-07 14:38:13 -0700

    Serialize a single artist

--- app/server/mono/curator/views.py
+++ app/server/mono/curator/views.py
@@ -4,37 +4,37 @@ from django.core import serializers
 from .models import Artist, Exhibition
 from .similar import Similar
 
+
 def exhibition(request, pk):
-    content = {
-        'artists': [],
-        'errors': None,
-        'title': None,
-        'url': None,
-    }
+    content = {"artists": [], "errors": None, "title": None, "url": None}
     exh = Exhibition.objects.get(pk=pk)
-    content['artists'] = serialize_artists(exh.artist_set.all())
-    content['title'] = exh.title
-    content['url'] = exh.moma_url
+    content["artists"] = serialize_artists(exh.artist_set.all())
+    content["title"] = exh.title
+    content["url"] = exh.moma_url
     return JsonResponse(content)
+
 
 def similar(request, token):
     tokens = Similar().get_ten(token)
     artists = get_artists_from_tokens(tokens)
-    content = {
-        'original_token': token,
-        'artists': serialize_artists(artists),
-    }
+    content = {"original_token": token, "artists": serialize_artists(artists)}
     return JsonResponse(content)
+
 
 def get_artists_from_tokens(tokens):
     return [Artist.objects.get(token=t) for t in tokens]
 
+
 def serialize_artists(artists):
     content = []
     for a in artists:
-        content.append({
-            'display_name': a.display_name,
-            'token': a.token,
-            'moma_url': a.moma_url,
-        })
+        content.append(serialize_artist(a))
     return content
+
+
+def serialize_artist(artist):
+    return {
+        "display_name": artist.display_name,
+        "token": artist.token,
+        "moma_url": artist.moma_url,
+    }
