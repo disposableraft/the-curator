7a49eda715caf743c2e230e66b9197deb95aab2a
Lance W <50839441+disposableraft@users.noreply.github.com>
2020-07-07 08:53:37 -0700

    Clean up import exhibition script

--- bin/import_to_graph.py
+++ bin/import_to_graph.py
@@ -5,39 +5,40 @@ from exhibition import Exhibition
 
 # get one exhibition with N artists
 
-G = Graph()
+def import_exhibition(graph, number, dataframe):
+    # Get the group of rows with `number` and have the artist role.
+    ex_df = dataframe.loc[dataframe["ExhibitionNumber"] == str(number)]
+    ex_df = ex_df.loc[df['ExhibitionRole'] == 'Artist']
+    
+    # Set up and create the exhibition node
+    # The first argument of `at` is the Series index, which can begin anywhere from 1 to N.
+    id = ex_df.at[ex_df.index[0], 'ExhibitionNumber']
+    title = ex_df.at[ex_df.index[0], 'ExhibitionTitle']
+    ex_node = Exhibition(int(id), title)
+
+    rows = ex_df[[
+        'WikidataID', 
+        'DisplayName', 
+        ]]
+
+    for _index, row in rows.iterrows():
+        # Set up and create the artist node
+        name = row['DisplayName']
+        wikidataID = row['WikidataID']
+        artist_node = Artist(name, wikidataID=wikidataID)
+        # Create an edge from exhibition to artist
+        ex_node.add_edge(artist_node)
+    
+    # Add the exhibition node to the graph
+    graph.add(ex_node) 
+
+    return graph
+
 
 csv_path = "~/data1/moma/exhibitions/MoMAExhibitions1929to1989.csv"
 df = pd.read_csv(csv_path, encoding="ISO-8859-1")
 
-# The ID
-exhibition = df.loc[df["ExhibitionNumber"] == str(1)]
-# Don't list curators, etc
-exhibition = exhibition.loc[df['ExhibitionRole'] == 'Artist']
+G = import_exhibition(Graph(), 2, df)
 
-rows = exhibition[[
-    'ConstituentID', 
-    'WikidataID', 
-    'DisplayName', 
-    'ExhibitionNumber', 
-    'ExhibitionTitle'
-    ]]
-
-
-id = exhibition.at[1, 'ExhibitionNumber']
-id = int(id)
-title = exhibition.at[1, 'ExhibitionTitle']
-
-exhibition = Exhibition(id, title)
-
-for _index, row in rows.iterrows():
-    name = row['DisplayName']
-    wikidataID = row['WikidataID']
-    node = Artist(name, wikidataID=wikidataID)
-    
-    exhibition.add_edge(node)
-
-G.add(exhibition) 
-
-print(G.bfs())
+print(G.nodes)
 
