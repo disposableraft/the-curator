e318f6f05edd25fd7ff66b02b9a66aaa4e55e129
Lance W <50839441+disposableraft@users.noreply.github.com>
2020-07-12 10:09:34 -0700

    Draw a graph with artist nodes filtered by degree

--- bin/draw_exhibition_network.py
+++ bin/draw_exhibition_network.py
@@ -1,19 +1,20 @@
 import constants
-from pygraphviz import AGraph 
+from pygraphviz import AGraph
 import utils
 
-file = constants.PROJECT_DATA_PICKLES.joinpath('2020710-moma-exhibitions.pickle')
+file = constants.PROJECT_DATA_PICKLES.joinpath('2020712-moma-exhibitions.pickle')
 data = utils.load_graph(file)
+G4 = AGraph()
 
-G4 = AGraph()    
+nodes_of_type = data.get_nodes()
 
-for node in data:
-    if len(node.edges) > 1 and len(node.edges) < 6:
+for _token, node in nodes_of_type['Exhibition'].items():
+    if node.degrees > 0:
         G4.add_node(node.id, shape='point', color='black')
         for m in node.edges:
-            if m:
+            if m and data[m].degrees == 25:
                 G4.add_node(m, shape='point', color='red')
-                G4.add_edge(node.id, m, color="#CCCCCC")
+                G4.add_edge(node.id, m, color="#CCCCCC", alpha=0.1)
 
 G4.layout()
 image = constants.PROJECT_DATA_IMAGES.joinpath('raw-exhibition-data.png')
