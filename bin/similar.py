737f884ab39b7544b8a7afb5c53c2bc7e750c0c0
Lance W <50839441+disposableraft@users.noreply.github.com>
2020-07-12 10:10:58 -0700

    Add similar artists from word2vec model

---
+++ bin/similar.py
@@ -0,0 +1,31 @@
+import utils
+import constants as c
+from graph import Node
+from gensim.models import Word2Vec
+
+class Similar(Node):
+    def __init__(self, id):
+        pass
+
+# Load the model
+# Load the graph
+# For each artist node in the graph
+#   Query word2vec model for similar artists
+#   Record the similar artist token and cosim in a dictionary of the artist node in the graph
+# Save the updated graph
+
+model = Word2Vec.load(str(c.WORD2VEC_MODEL))
+pickled_graph = c.PROJECT_DATA_PICKLES.joinpath('2020712-moma-exhibitions.pickle')
+graph = utils.load_graph(pickled_graph)
+
+nodes_of_type = graph.get_nodes()
+artists = nodes_of_type['Artist'].items()
+
+for _token, node in artists:
+    try:
+        # Top ten is sort of arbitrary. Is there a way to select by similarity?
+        node.similar = model.wv.most_similar(node.id, topn=10)
+    except KeyError as err:
+        print(f'Error selecting similar artist: {err}')
+
+utils.save_graph(graph, c.PROJECT_DATA_PICKLES.joinpath('2020712-moma-word2vec.pickle'))
