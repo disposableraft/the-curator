e60f562a2b937c0e81b9283fe721afe0b9dae4e5
Lance W <50839441+disposableraft@users.noreply.github.com>
2020-07-12 19:37:22 -0700

    Fetching wikidata records:
    - Only fetch labels for nodes with > 2 degrees
    - Save tmp files along the way

--- bin/fetch_movements.py
+++ bin/fetch_movements.py
@@ -3,7 +3,7 @@ import constants as c
 from category import Category
 from wikidata import Wikidata
 
-path = c.PROJECT_DATA_PICKLES.joinpath('202078-moma-exhibitions.pickle')
+path = c.PROJECT_DATA_PICKLES.joinpath('2020712-moma-exhibitions.pickle')
 graph = utils.load_graph(path)
 
 report = {
@@ -14,15 +14,24 @@ report = {
 
 artist_nodes = [value for value in graph
                 if value.type == 'Artist'
-                and value.wikidataID != None]
+                and value.wikidataID != None
+                and value.degrees > 2]
 
-for artist_node in artist_nodes:
-    labels = Wikidata(artist_node.wikidataID).stub_fetch()
-    if len(labels) == 0:
+print(f'Total artist nodes: {len(artist_nodes)}')
+
+for index, artist_node in enumerate(artist_nodes):
+    if index % 50 == 0:
+        print(f'Saving tmp file {index / 50}...')
+        utils.save_graph(graph, c.PROJECT_DATA_PICKLES.joinpath('tmp-fetch-movements.pickle'))
+        utils.save_graph(report, c.PROJECT_DATA_PICKLES.joinpath('tmp-fetch-movements-report.pickle'))
+    if str(artist_node.wikidataID) == 'nan':
+        continue
+    labels = Wikidata(artist_node.wikidataID).fetch()
+    if not labels:
         report['artists_not_updated'].add(artist_node.id)
     else:
-        artist_node.categories.add(labels)
         for label in labels:
+            artist_node.categories.add(label)
             category_id = hash(label)
             if category_id not in graph.nodes:
                 graph.add(Category(label))
