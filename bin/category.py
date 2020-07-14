07cf7e702362991c9338a56e96465e2d65650ef5
Lance W <50839441+disposableraft@users.noreply.github.com>
2020-07-13 17:04:04 -0700

    Don't use hash for Category.id

--- bin/category.py
+++ bin/category.py
@@ -3,5 +3,5 @@ from graph import Node
 class Category(Node):
     def __init__(self, title):
         self.title = title
-        self.id = hash(title)
+        self.id = title
         super().__init__(self.id)
