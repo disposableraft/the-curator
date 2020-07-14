97e01275b9a59ef95b66ce6e5374bd753cb09021
Lance W <50839441+disposableraft@users.noreply.github.com>
2020-07-13 17:05:18 -0700

    Path updates and crappy pearson_r function

--- bin/constants.py
+++ bin/constants.py
@@ -5,9 +5,11 @@ USER_PATH = Path('~/').expanduser()
 MOMA_DATA = USER_PATH.joinpath('data1/moma')
 MOMA_EXHIBITIONS_CSV = MOMA_DATA.joinpath('exhibitions/MoMAExhibitions1929to1989.csv')
 
+
 PROJECT_PATH = USER_PATH.joinpath('writing1/projects/the-curator')
 PROJECT_BIN = PROJECT_PATH.joinpath('bin')
 PROJECT_DATA = PROJECT_PATH.joinpath('data')
+PROJECT_TMP = PROJECT_DATA.joinpath('tmp')
 PROJECT_DATA_IMAGES = PROJECT_PATH.joinpath('images')
 PROJECT_DATA_MODELS = PROJECT_DATA.joinpath('models')
 PROJECT_DATA_PICKLES = PROJECT_DATA.joinpath('pickles')
