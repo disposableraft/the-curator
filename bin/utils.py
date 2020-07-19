7372abdd32359c2c6e243d8676b5f78344f7dc0f
Lance W <50839441+disposableraft@users.noreply.github.com>
2020-07-19 14:23:15 -0700

    Remove pearson fn

--- bin/utils.py
+++ bin/utils.py
@@ -1,5 +1,4 @@
 import pickle
-import math
 import matplotlib.pyplot as plt
 
 def load_graph(file):
@@ -26,24 +25,4 @@ def draw_boxplot(X, show=False, **kwargs):
         plt.show()
     else:
         return boxplot
-
-# Yikes! Please rewrite
-def pearson_r(X, Y):
-    N = len(X)
-    # numerator expressions
-    xy = zip(X, Y)
-    sum_xy = sum([x*y for x, y in xy])
-
-    mean_sums_x_y = (sum(X) * sum(Y)) / N
-
-    #denominator expressions
-    sum_x2 = sum([x**2 for x in X])
-    mean_sumx_2 = math.pow(sum(X), 2) / N
-    sum_x2_minus_mean = sum_x2 - mean_sumx_2
-
-    sum_y2 = sum([y**2 for y in Y])
-    mean_sumy_2 = math.pow( sum(Y), 2 ) / N
-    sum_y2_minus_mean = sum_y2 - mean_sumy_2
-
-    return (sum_xy - mean_sums_x_y) / ( math.sqrt(sum_x2_minus_mean) * math.sqrt(sum_y2_minus_mean) )
 
