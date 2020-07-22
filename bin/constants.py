21174f7c56106a834e1ad256c171adc12afa57b7
Lance W <50839441+disposableraft@users.noreply.github.com>
2020-07-22 10:54:22 -0700

    Add and update paths

--- bin/constants.py
+++ bin/constants.py
@@ -7,6 +7,7 @@ MOMA_EXHIBITIONS_CSV = MOMA_DATA.joinpath('exhibitions/MoMAExhibitions1929to1989
 
 PROJECT = USER_PATH.joinpath('writing1/projects/the-curator')
 DATA = PROJECT.joinpath('data')
+TRAIN = DATA.joinpath('train')
 PROJECT_TMP = DATA.joinpath('tmp')
 
 PROJECT_DATA_IMAGES = DATA.joinpath('images')
@@ -16,4 +17,4 @@ PICKLES = DATA.joinpath('pickles')
 VERSIONS = PICKLES.joinpath('versions')
 CURRENT = PICKLES.joinpath('versions/current.pickle')
 
-WORD2VEC = MODELS.joinpath('moma-combos.model')
+WORD2VEC = MODELS.joinpath('20200721.word2vec')
