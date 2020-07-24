bf2c5d3872d9060bb7a4e6b22098383bcf3049bd
Lance W <50839441+disposableraft@users.noreply.github.com>
2020-07-24 11:08:14 -0700

    Report in JSON, not markdown

--- bin/report.py
+++ bin/report.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+import json
 import utils
 from scipy import stats
 import matplotlib.pyplot as plt
@@ -13,7 +14,6 @@ report_dir = c.DATA.joinpath('report-02')
 title = 'Report 01 for 20200721-skipgram.word2vec'
 model_name = '20200721-skipgram.word2vec'
 dataset = 'train-01'
-# TODO possibly look for this dir and create if necessary
 """
 /CONFIGS
 """
@@ -24,38 +24,9 @@ try:
 except FileExistsError:
     print(f'Directory exists: {report_dir}')
 
-"""
-The graph and the model
-"""
-
-# Assumes current graph file
-graph = utils.load_graph()
-types = graph.get_nodes()
-categories = types['Category']
-exhibitions = types['Exhibition']
-artists = types['Artist']
-
-# Graph name
-graph_path = os.readlink(c.CURRENT)
-graph_name = graph_path.split('/')[-1]
-
-# The model
-with open(c.MODELS.joinpath(f'{model_name}-notes'), 'rb') as f:
-    model_notes = pickle.load(f)
-
-# Labeled and Unlabeled
-labeled_artists = set()
-for c in categories.values():
-    for m in c.edges:
-        labeled_artists.add(m)
-artists_degree_gt_1 = set([a.id for a in artists.values() if a.degrees > 2])
-unlabeled_artists = artists_degree_gt_1 ^ labeled_artists
-
-# Error rates
-
-def get_labeled_nodes(cat):
+def get_labeled_nodes(cats):
     labeled = set()
-    for c in cat:
+    for c in cats:
         for m in c.edges:
             labeled.add(m)
     return labeled
@@ -103,41 +74,95 @@ def score_category(graph, cat, all_labeled_nodes):
         'total_hits_misses': len(hits) + len(misses)
     }
 
-def report_error_rates(lines):
-    output = str()
-    output += f'{"Category":<15s} {"Mean":>14s} {"Variance":>14s} {"N":>3s}\n'
-    output += '---------------------------------------------------\n'
+def report_error_rates(lines, report_dict):
+    errs = report_dict['results']['error_rates']
     for line in lines:
-        output += f"{line['cat']:<25s} {line['mean']:<10f} {line['variance']:<10f} {len(line['scores']):<10d}\n"
-    return output
+        cat = line['cat']
+        errs[cat] = {}
+        errs[cat]['mean'] = line['mean']
+        errs[cat]['variance'] = line['variance']
+        errs[cat]['scores'] = line['scores']
+    return report_dict
+
+"""
+The report dict is the star of the show.
+"""
+report = {
+    'title': title,
+    'graph': {},
+    'model': {
+        'name': model_name,
+        'dataset': dataset
+    },
+    'results': {
+        'linregress': {},
+        'error_rates' : {
+        },
+    }
+}
+
+# Assumes current graph file
+graph = utils.load_graph()
+types = graph.get_nodes()
+categories = types['Category'].values()
+exhibitions = types['Exhibition']
+artists = types['Artist'].values()
+
+# Labeled and Unlabeled
+# TODO this is same as `get_labeled_nodes()`
+labeled_artists = get_labeled_nodes(categories)
+artists_degree_gt_1 = set([a.id for a in artists if a.degrees > 2])
+unlabeled_artists = artists_degree_gt_1 ^ labeled_artists
+
+# Graph name
+graph_path = os.readlink(c.CURRENT)
+report['graph']['name'] = graph_path.split('/')[-1]
+report['graph']['vertices'] = graph.count_edges()
+report['graph']['nodes'] = graph.count_nodes()
+report['graph']['density'] = graph.density()
+report['graph']['exhibitions']  = len(exhibitions)
+report['graph']['categories']  = len(categories)
+report['graph']['artists']  = len(artists)
+report['graph']['unlabeled_artists']  = len(unlabeled_artists)
+report['graph']['labeled_artists']  = len(labeled_artists)
+
+# Open model's report
+# TODO the model provides some useful stats on vocab, etc
+# TODO use json for gawds sakes!
+with open(c.MODELS.joinpath(f'{model_name}-notes'), 'rb') as f:
+    model_notes = pickle.load(f)
+
+report['model']['sg'] = model_notes['sg']
+report['model']['workers'] = model_notes['workers']
+report['model']['size'] = model_notes['size']
+report['model']['min_count'] = model_notes['min_count']
+report['model']['epochs'] = model_notes['epochs']
 
 # Error rates per category
-labeled_nodes = get_labeled_nodes(categories.values())
+labeled_nodes = get_labeled_nodes(categories)
 err_by_category = []
-
-for category in categories.values():
+for category in categories:
     if category.degrees > 1:
         score = score_category(graph, category, labeled_nodes)
         if score:
             err_by_category.append(score)
+report = report_error_rates(err_by_category, report)
 
 # Error rates total
 all_scores = []
-
-for category in categories.values():
+for category in categories:
     if category.degrees > 1:
         cat_score = score_category(graph, category, labeled_nodes)
         if cat_score:
             for x in cat_score['scores']:
                 all_scores.append(x)
-
 err_total = {
     'cat': 'all',
     'mean': mean(all_scores),
     'variance': variance(all_scores),
     'scores': all_scores
-#     Add hits and misses and cat degrees to reports?
 }
+report = report_error_rates([err_total], report)
 
 # Error Rate Histogram
 plt.title('Error rate distribution')
@@ -158,9 +183,8 @@ plt.scatter(corr_X, corr_Y, alpha=0.2)
 plt.savefig(corr_scatter)
 plt.close()
 
-lev_mode = 'median'
-lev_statistic, lev_pvalue = stats.levene(corr_X, corr_Y, center=lev_mode)
-lr_slope, lr_intercept, lr_rvalue, lr_pvalue, lr_stderr = stats.linregress(corr_X, corr_Y)
+lg = stats.linregress(corr_X, corr_Y)
+report['results']['linregress'] = {k:v for k,v in zip(lg._fields, lg)}
 
 # Category Graphs
 category_graphs = str()
@@ -173,86 +197,6 @@ for x in err_by_category:
         category_graphs += (f'![](./{image_name})\n\n')
 
 
-"""
-------------
-THE TEMPLATE
-------------
-"""
-markdown = f"""
-
-# {title}
-
-### Data: {dataset}
-
-### Graph: {graph_name}
-
-- edges: {graph.count_edges()}
-- vertices: {graph.count_nodes()}
-- density: {graph.density()}
-- exhibition: {len(exhibitions)}
-- category: {len(categories)}
-- artist: {len(artists)}
-- edges between artist nodes: TK?
-- unlabeled: {len(unlabeled_artists)}
-- labeled: {len(labeled_artists)}
-- percent of labeled artists (with degrees > 2): {len(labeled_artists) / (len(unlabeled_artists) + len(labeled_artists))}
-
-### Model: {model_name}
-
-Params:
-
-- sg: {model_notes['sg']}
-- workers: {model_notes['workers']}
-- size: {model_notes['size']}
-- min_count: {model_notes['min_count']}
-- epochs: {model_notes['epochs']}
-
-## Error Rates
-
-### Total
-
-```txt
-{report_error_rates([err_total])}
-```
-
-### By category (where category.degrees > N):
-
-```txt
-{report_error_rates([x for x in err_by_category if len(x['scores']) > 9])}
-```
-
-## Error Rate Distribution
-
-![all scores](./error-distribution.png)
-
-## Correlation between category error rate and category.degrees
-
-![correlation scatter plot](./{corr_scatter_name})
-
-## Regressions Results​
-
-### Levene Test
-
-- mode: {lev_mode}
-- stat: {lev_statistic}
-- p-value: {lev_pvalue}
-
-### Linear Regression
-
-- slope: {lr_slope}
-- intercept: {lr_intercept}
-- r-value: {lr_rvalue}
-- p-value: {lr_pvalue}
-- std error: {lr_stderr}
-
-### Category Networks
-
-(To be included, a category must have > N members.)
-
-{category_graphs}
-
-"""
-
 # And, finally!
-with open(report_dir.joinpath('report.md'), 'w') as f:
-    f.write(markdown)
+with open(report_dir.joinpath('report.json'), 'w') as f:
+    f.write(json.dumps(report))
