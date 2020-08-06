b995f876da4efb9e97d5977939bcf169a1de4af5
Lance W <50839441+disposableraft@users.noreply.github.com>
2020-08-05 20:29:30 -0700

    Use UUID for exhibition id
    With multiple datasets, there could be many identical IDs.
    Keep the source ID for reference.

--- bin/exhibition.py
+++ bin/exhibition.py
@@ -1,6 +1,9 @@
 from graph import Node
+import uuid
 
 class Exhibition(Node):
-    def __init__(self, id, title):
-        super().__init__(id)
+    def __init__(self, id, title, source):
+        super().__init__(str(uuid.uuid4()))
         self.title = title
+        self.source = source
+        self.source_id = id
