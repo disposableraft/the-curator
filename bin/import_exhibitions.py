0eac517ff7b95823b001e525c85f2262bac7c4ca
Lance W <50839441+disposableraft@users.noreply.github.com>
2020-08-05 20:35:38 -0700

    Refactor importers and add tests

--- bin/import_exhibitions.py
+++ bin/import_exhibitions.py
@@ -1,3 +1,4 @@
+import unittest
 import constants
 import utils
 import pandas as pd
@@ -5,49 +6,156 @@ from graph import Graph
 from artist import Artist
 from exhibition import Exhibition
 
+class Importer:
+    def __init__(self, *importers):
+        self.graph = Graph()
+        self.importers = list(importers)
+
+    def run(self):
+        for importer in self.importers:
+            for row in importer:
+                (id, title, source), artists = row
+                print(f'Importing "{title}" from {source}')
+                exh_node = self.graph.add(Exhibition(id, title, source))
+                nodes = []
+                for a in artists:
+                    name, wikidata_id = a
+                    node = self.graph.add(Artist(name, wikidataID=wikidata_id))
+                    nodes.append(node)
+                self.graph.add_edges(exh_node, nodes)
+
+class Dome:
+    def __init__(self, artist_path, exh_path):
+        self.artist_df = pd.read_csv(artist_path)
+        self.exh_df = pd.read_csv(exh_path)
+        self.source = 'dome'
+        self.exhibitions = self.load_data()
+
+    def __iter__(self):
+        yield from self.exhibitions
+
+    def load_data(self):
+        exhibitions = []
+        for _index, exh in self.exh_df.iterrows():
+            exhibitions.append(self.build_exhibition(exh))
+        return exhibitions
+
+    def transform_name(self, name):
+        names = name.split(',')
+        names.reverse()
+        return ' '.join(names).strip()
+
+    def build_exhibition(self, exh):
+        id = exh.ID
+        title = exh.Title
+        members = self.artist_df.loc[self.artist_df.ExhibitionID == id]
+        artists = []
+        for _, a in members.iterrows():
+            name = self.transform_name(a.Name)
+            artists.append((name, a.Wikidata))
+        # [(id, title, source), [(name, wikidata_id), ...]]
+        return [(id, title, self.source), artists]
+
 class Moma:
-    def __init__(self, dataframe_path):
-        self.df = self.read_csv(dataframe_path)
+    def __init__(self, path):
+        self.df = pd.read_csv(path, encoding="ISO-8859-1")
+        self.source = 'moma'
+        self.exhibitions = self.load_data()
 
-    def list_exhibitions(self):
+    def __iter__(self):
+        yield from self.exhibitions
+
+    def load_data(self):
+        exhibitions = []
+        for id in self.get_ids():
+            exhibition = self.get_exhibition(id)
+            if exhibition:
+                exhibitions.append(exhibition)
+        return exhibitions
+
+    def get_ids(self):
         return list(self.df.ExhibitionID.unique())
-    
-    def read_csv(self, path):
-        return pd.read_csv(path, encoding="ISO-8859-1")
 
-    def graph_exhibition(self, graph, ex_number):
-        # Get the group of rows with `number` and have the artist role.
-        ex_df = self.df.loc[self.df["ExhibitionID"] == ex_number]
-        ex_df = ex_df.loc[ex_df['ExhibitionRole'] == 'Artist']
+    def get_exhibition(self, exh_id):
+        # [(id, title, source), [(name, wikidata_id), ...]]
+        exhibition = self.df.loc[self.df['ExhibitionID'] == exh_id]
+        if len(exhibition) < 2:
+            return False
+        id = exhibition.ExhibitionID.to_list()[0]
+        title = exhibition.ExhibitionTitle.to_list()[0]
+        artists = []
+        for _i, row in exhibition.iterrows():
+            artists.append((row['DisplayName'], row['WikidataID']))
+        return [(id, title, self.source), artists]
 
-        count, _cols = ex_df.shape
-        if ex_df.empty or count < 2 or count > 50:
-            return
 
-        # Set up and create the exhibition node
-        # The first argument of `at` is the Series index, which can begin anywhere from 1 to N.
-        id = ex_df.at[ex_df.index[0], 'ExhibitionID']
-        title = ex_df.at[ex_df.index[0], 'ExhibitionTitle']
-        # Some exhibition numbers contain string values :(, so leave `id` as string
-        ex_node = Exhibition(int(id), title)
-
-        # Add the exhibition node to the graph
-        graph.add(ex_node)
-
-        for _index, row in ex_df.iterrows():
-            # Set up and create the artist node
-            name = row['DisplayName']
-            wikidataID = row['WikidataID']
-            artist_node = graph.add(Artist(name, wikidataID=wikidataID))
-            # Create an edge from exhibition to artist
-            graph.add_edge(ex_node.id, artist_node)
-
-        return graph
-    
 def run(config):
     moma = Moma(constants.MOMA_EXHIBITIONS_CSV)
-    graph = Graph()
-    for exhibition_number in moma.list_exhibitions():
-        moma.graph_exhibition(graph, exhibition_number)
-    fn = utils.save_graph(graph, 'import.pickle', config)
-    print(f'Imported MoMA Exhibitions data to {fn}')
+    dome = Dome(
+        constants.DOME_ARTISTS_CSV,
+        constants.DOME_EXHIBITIONS_CSV
+        )
+    importer = Importer(moma, dome)
+    importer.run()
+    utils.save_graph(importer.graph, 'import.pickle', config)
+
+class TestImporter(unittest.TestCase):
+    def test_importer_init(self):
+        moma = Moma(constants.MOMA_EXHIBITIONS_CSV_TEST)
+        importer = Importer(moma)
+        self.assertEqual(len(importer.importers), 1)
+
+    def test_importer_run(self):
+        moma = Moma(constants.MOMA_EXHIBITIONS_CSV_TEST)
+        dome = Dome(
+            constants.DOME_ARTISTS_CSV_TEST,
+            constants.DOME_EXHIBITIONS_CSV_TEST
+        )
+        importer = Importer(moma, dome)
+        importer.run()
+        self.assertEqual(importer.graph.count_nodes(), 47)
+        self.assertEqual(importer.graph.count_edges(), 43)
+
+class TextDome(unittest.TestCase):
+    def test_transform_name(self):
+        name = 'Gauguin, Paul'
+        expected = 'Paul Gauguin'
+        self.assertEqual(Dome.transform_name(dict, name), expected)
+
+    def test_dome_init(self):
+        dome = Dome(
+            constants.DOME_ARTISTS_CSV_TEST,
+            constants.DOME_EXHIBITIONS_CSV_TEST
+        )
+        self.assertEqual(dome.source, 'dome')
+        self.assertEqual(len(dome.exhibitions), 3)
+
+    def test_dome_load_iter(self):
+        dome = Dome(
+            constants.DOME_ARTISTS_CSV_TEST,
+            constants.DOME_EXHIBITIONS_CSV_TEST
+            )
+        artists = []
+        for exh in dome:
+            (_), members = exh
+            for m in members:
+                artists.append(m)
+        self.assertEqual(len(artists), 20)
+
+class TestMoma(unittest.TestCase):
+    def test_moma_init(self):
+        moma = Moma(constants.MOMA_EXHIBITIONS_CSV_TEST)
+        self.assertEqual(moma.source, 'moma')
+        self.assertEqual(len(moma.exhibitions), 3)
+
+    def test_moma_load_iter(self):
+        moma = Moma(constants.MOMA_EXHIBITIONS_CSV_TEST)
+        artists = []
+        for exh in moma:
+            (_), members = exh
+            for m in members:
+                artists.append(m)
+        self.assertEqual(len(artists), 23)
+
+if __name__ == '__main__':
+    unittest.main()
